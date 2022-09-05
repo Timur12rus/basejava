3a943bb5d288a52528d3280129e5fe02586af728
Timur Gordeev <tim.gordeev2015@yandex.ru>
2022-09-05 14:33:08 +0300

    homework 01

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +8,76 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < 10000; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < 10000; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < 10000; i++) {
+            if (storage[i] != null && storage[i].uuid == uuid) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        try {
+            int k = 0;
+            for (int i = 0; i < 10000; i++) {
+                if (uuid == storage[i].uuid) {
+                    storage[i] = null;
+                    k = i;
+                    break;
+                }
+            }
+            for (int i = k; i < storage.length - 1; i++) {
+                storage[i] = storage[i + 1];
+            }
+            storage[storage.length - 1] = null;
+        } catch (NullPointerException e) {
+
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int size = 0;
+        for (int i = 0; i < 10000; i++) {
+            if (storage[i] != null) {
+                size++;
+            } else {
+                break;
+            }
+        }
+        Resume[] array = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = storage[i];
+        }
+        return array;
     }
 
     int size() {
-        return 0;
+        int resumeCount = 0;
+        for (int i = 0; i < 10000; i++) {
+            if (storage[i] != null) {
+                resumeCount++;
+            } else {
+                break;
+            }
+        }
+        return resumeCount;
     }
 }
